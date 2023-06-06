704299f31e418334d633a43628533a97c5dbf214
Tejaswari Behera <tejaswaribehera@gmail.com>
2023-06-06 15:05:25 +0530

    Add files via upload

---
+++ crwn/src/App.jsx
@@ -0,0 +1,24 @@
+//react imports
+import { Routes, Route } from 'react-router-dom';
+
+//Components
+import Navigation from './routes/navigation/navigation.component';
+import Home from './routes/home/home.component';
+import Shop from './routes/shop/shop.component';
+import Authentication from './routes/authentication/authentication.component';
+
+//Styles
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
